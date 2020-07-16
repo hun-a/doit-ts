54c6401321c419905c4657b3d642ad715a26e673
hun-a <seunghun.chan@gmail.com>
2020-07-17 00:24:06 +0900

    Create react using typescript

---
+++ ch12-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {IUser} from './models/IUser'
+import {getDataPromise} from './getDataPromise'
+
+const App: React.FC = () => {
+  getDataPromise((users: IUser[]) => console.log('users', users))(0, 1)
+  return (
+    <div className="App">Please open console window!</div> 
+  )
+}
+
+export default App;
